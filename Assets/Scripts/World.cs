ddbd7cdd0e9d21ec69d4fcf2397ba88ed6f4c34c
Adam Keenan <adam.g.keenan@gmail.com>
2015-03-07 03:30:42 -0500

    Loads one chunk with two non-air blocks

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World : MonoBehaviour {
+    public Dictionary<WorldPos, Chunk> chunks = new Dictionary<WorldPos, Chunk>();
+
+    public GameObject chunkPrefab;
+
+    void Start () {
+
+    }
+}
